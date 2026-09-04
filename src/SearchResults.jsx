7522e2430c7c0dff331ca8a10214644d8bef1b8b
Краєвський Олексій Андрійович <vt241_koa@student.ztu.edu.ua>
2026-09-04 13:12:38 +0300

    feat: added scroll to search results

--- src/SearchResults.jsx
+++ src/SearchResults.jsx
@@ -15,21 +15,22 @@ function SearchResults(){
     const tracks = use(searchPromise);
 
     if (tracks.length === 0) {
-        return <div className='search-results'><h2>Нічого не знайдено</h2></div>;
+        return <div className='search-results-container'><h2>Нічого не знайдено</h2></div>;
     }
 
     return(
-        <div className="search-results">
-            <h2>Результати пошуку</h2>
-            {tracks.map(track=>
-                <TrackCard
-                key={track.id}
-                id={track.id}
-                artistName={track.artist_name}
-                songName={track.name}
-                imageUrl={track.image}
-                audioUrl={track.audio}/>
+        <div className="search-results-container">
+            <div className='search-results'>
+                {tracks.map(track=>
+                    <TrackCard
+                        key={track.id}
+                        id={track.id}
+                        artistName={track.artist_name}
+                        songName={track.name}
+                        imageUrl={track.image}
+                        audioUrl={track.audio}/>
                 )}
+            </div>
         </div>
     )
 }
